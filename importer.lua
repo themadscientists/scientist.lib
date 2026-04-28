157caab2e187d6d4ab4227f389d45968e9ba39aa
themadscientists <139097481+themadscientists@users.noreply.github.com>
2026-04-27 22:02:05 -0400

    implimented importer functionality

--- importer.lua
+++ importer.lua
@@ -1,3 +1,31 @@
-local import = function(file: string)
-    
+safegetgenv = function()
+    if getgenv then
+        return getgenv()
+    else
+        return {}
+    end
 end
+
+local import = function(file: string)
+    if typeof(file) ~= 'string' then
+        return
+    end
+
+    if file == 'importer' then
+        return
+    end
+
+    local Success, Data = pcall(function()
+        return game:HttpGet('https://raw.githubusercontent.com/themadscientists/scientist.lib/refs/heads/main/'..file..'.lua')
+    end)
+
+    if not Success then
+        return warn('Failed to import ' .. file .. '!')
+    end
+
+    return loadstring(Data);
+end
+
+shared.import = import
+_G.import = import
+safegetgenv().import = import
